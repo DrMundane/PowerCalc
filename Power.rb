465615e7123b58197b4c3f5fad2166e9a1291e31
Nick <nicholas.lashinsky@gmail.com>
2015-08-05 14:42:17 -0400

    Create Power.rb
    The important stuff

---
+++ Power.rb
@@ -0,0 +1,15 @@
+#Ruby program for calculating power with many lights with the same power draw
+#Maybe I'll add support for more than 1 level of power later...
+
+print "Input Number of Lights: "
+lights = gets.chomp
+print "Input Energy in Watts per Light: "
+energy = gets.chomp
+print "Input 120/240 Service: "
+service = gets.chomp
+
+totalwatt = Float(energy)*Float(lights)
+
+amps = Float(totalwatt)/Float(service)
+
+puts "#{lights} lights will use #{totalwatt} watts of power, or #{amps} amps, on a #{service} service"
